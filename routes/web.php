33d4eb575c4f70808ae0fb7a068dd6b5631de0f1
santhusha27 <94159141+santhusha27@users.noreply.github.com>
2026-07-11 03:15:21 +0530

    role function add

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\SupplierController;
 use App\Http\Controllers\StockController;
 use App\Http\Controllers\PurchaseController;
 use App\Http\Controllers\SaleController;
+use App\Http\Controllers\ReportController;
 use App\Models\Product;
 use App\Models\Stock;
 use App\Models\Supplier;
@@ -65,8 +66,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
             'chartStocks' => $chartStocks,
         ]);
     })->name('dashboard');
-    // Roles (Admin)
-   Route::get('/roles', [RoleController::class, 'index'])->name('roles.index');
+   
 
     // Users (Admin)
    Route::middleware('role:admin')->group(function () {
@@ -74,49 +74,69 @@ Route::middleware(['auth', 'verified'])->group(function () {
         Route::post('/users', [UserController::class, 'store'])->name('users.store');
         Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
         Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+         // Roles (Admin)
+        Route::get('/roles', [RoleController::class, 'index'])->name('roles.index');
     });
 
     // Categories
+    Route::middleware('role:admin,storekeeper')->group(function () {
         Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
         Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
         Route::put('/categories/{id}', [CategoryController::class, 'update'])->name('categories.update');
         Route::delete('/categories/{id}', [CategoryController::class, 'destroy'])->name('categories.destroy');
+    });
     // Products
-    Route::middleware('role:admin,storekeeper')->group(function () {
+    Route::middleware('role:admin,storekeeper,staff')->group(function () {
         Route::get('/products', [ProductController::class, 'index'])->name('products.index');
         Route::post('/products', [ProductController::class, 'store'])->name('products.store');
         Route::put('/products/{id}', [ProductController::class, 'update'])->name('products.update');
         Route::delete('/products/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
     });
     // Suppliers
+    Route::middleware('role:admin,storekeeper')->group(function () {
         Route::get('/suppliers', [SupplierController::class, 'index'])->name('suppliers.index');
         Route::post('/suppliers', [SupplierController::class, 'store'])->name('suppliers.store');
         Route::put('/suppliers/{id}', [SupplierController::class, 'update'])->name('suppliers.update');
         Route::delete('/suppliers/{id}', [SupplierController::class, 'destroy'])->name('suppliers.destroy');
-
+    });
     // Stock (View Only)
-    Route::get('/stocks', [StockController::class, 'index'])
-        ->name('stocks.index');
-
+    Route::middleware('role:admin,storekeeper,staff')->group(function () {
+        Route::get('/stocks', [StockController::class, 'index'])->name('stocks.index');
+    });
     // Purchases (Stock IN)
-    Route::get('/purchases', [PurchaseController::class, 'index'])->name('purchases.index');
-    Route::post('/purchases', [PurchaseController::class, 'store'])->name('purchases.store');
-    Route::get('/purchases/{id}/grn', [PurchaseController::class, 'show'])->name('purchases.grn');
-    Route::post('/purchases/{id}/email-grn', [PurchaseController::class, 'emailGrn'])->name('purchases.emailGrn');
-    Route::delete('/purchases/{id}', [PurchaseController::class, 'destroy'])->name('purchases.destroy');
-
+    Route::middleware('role:admin,storekeeper')->group(function () {
+        Route::get('/purchases', [PurchaseController::class, 'index'])->name('purchases.index');
+        Route::post('/purchases', [PurchaseController::class, 'store'])->name('purchases.store');
+        Route::get('/purchases/{id}/grn', [PurchaseController::class, 'show'])->name('purchases.grn');
+        Route::post('/purchases/{id}/email-grn', [PurchaseController::class, 'emailGrn'])->name('purchases.emailGrn');
+        Route::delete('/purchases/{id}', [PurchaseController::class, 'destroy'])->name('purchases.destroy');
+    });
     // Sales (Stock OUT)
-    Route::get('/sales', [SaleController::class, 'index'])->name('sales.index');
-    Route::post('/sales', [SaleController::class, 'store'])->name('sales.store');
-    Route::get('/sales/{id}/invoice', [SaleController::class, 'invoice'])->name('sales.invoice');;    
-    Route::delete('/sales/{id}', [SaleController::class, 'destroy'])->name('sales.destroy');
-    Route::get('/sales/{id}/invoice-pdf', [SaleController::class, 'invoicePdf'])
-    ->name('sales.invoicePdf');
+    Route::middleware('role:admin,staff')->group(function () {
+        Route::get('/sales', [SaleController::class, 'index'])->name('sales.index');
+        Route::post('/sales', [SaleController::class, 'store'])->name('sales.store');
+        Route::get('/sales/{id}/invoice', [SaleController::class, 'invoice'])->name('sales.invoice');    
+        Route::delete('/sales/{id}', [SaleController::class, 'destroy'])->name('sales.destroy');
+        Route::get('/sales/{id}/invoice-pdf', [SaleController::class, 'invoicePdf'])->name('sales.invoicePdf');
+    });
+    
+
+   
+   
+
+        // Main Reports Page
+        Route::get('/reports', [ReportController::class, 'index'])->name('reports.index');
+        // Custom Date Sales Report
+        Route::get('/reports/custom', [ReportController::class, 'custom'])->name('reports.custom');
+
+
+
+});
 
     
    
 
 
-});
+
 
 require __DIR__.'/settings.php';
